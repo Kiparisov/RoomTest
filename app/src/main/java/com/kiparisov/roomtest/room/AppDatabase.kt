88637301e2847a7b9943c6ea6194926cd514d053
Kiparisov <bobbycrasby@yandex.ru>
2022-07-30 06:04:44 +0600

    room created

---
+++ app/src/main/java/com/kiparisov/roomtest/room/AppDatabase.kt
@@ -0,0 +1,36 @@
+package com.kiparisov.roomtest.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.kiparisov.roomtest.room.account.AccountDao
+import com.kiparisov.roomtest.room.account.AccountEntity
+
+
+@Database(entities = [AccountEntity::class], version = 1, exportSchema = false)
+abstract class AppDatabase: RoomDatabase() {
+    companion object{
+        private var db: AppDatabase? = null
+        private const val DB_NAME = "database.db"
+        private val LOCK = Any()
+
+
+        fun getInstance(context: Context): AppDatabase{
+            db?.let { return it }
+
+            synchronized(LOCK){
+                val instance: AppDatabase = Room.databaseBuilder(
+                    context,
+                    AppDatabase::class.java,
+                    DB_NAME
+                ).build()
+
+                db = instance
+                return instance
+            }
+        }
+    }
+
+    abstract fun getAccountDao(): AccountDao
+}
---
+++ app/src/main/java/com/kiparisov/roomtest/room/account/AccountDao.kt
@@ -0,0 +1,17 @@
+package com.kiparisov.roomtest.room.account
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface AccountDao {
+    @Insert
+    fun createAccount(accountEntity: AccountEntity)
+
+    @Query("SELECT * FROM accounts WHERE email = :email")
+    fun getAccountByEmail(email: String): AccountEntity
+
+    @Query("SELECT * FROM accounts WHERE id = :id")
+    fun getAccountById(id: Long): AccountEntity
+}
